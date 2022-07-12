68f88040def2e6373485234c2bc3d73a130d148e
Daniel Ivanov Dechev <datas2k@gmail.com>
2022-07-12 18:08:28 +0200

    1stc challenge done

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Dynamic Programing
 
 Solve Algorithmic Problems with Dynamic Programing
+
+Challenges:
+1. Calculate the 40th number of the fibonacci sequence.
+2. Count the number of different ways to move through a 6*9 grid.
+3. Given a set of coins, how can we make 27 cents in the least number coins.
+4. Given a set of substrings, what are the possible ways to construct the string 'potentpot' ?
--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("yeeee");
+const fib_brute = (n) => {
+  if (n <=2) return 1;
+  return fib_brute(n-1) + fib_brute(n-2);
+};
+
+const fib_memo = (n,memo = {}) => {
+  if (n in memo) return memo[n];
+  if (n <=2) return 1;
+  memo[n] = fib_memo(n-1, memo) + fib_memo(n-2,memo);
+  return memo[n];
+};
+
+console.log(fib_brute(2));
+console.log(fib_brute(7));
+console.log(fib_brute(8));
+console.log(fib_memo(7388)); // top of the stack
